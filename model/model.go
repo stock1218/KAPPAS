94eafbee6a8544600d38ae4eaf09c87f6d7eedd2
John Samuels <johnsamuels1218@gmail.com>
2019-08-06 16:39:55 -0400

    Create model package and add first passing test

---
+++ model/model.go
@@ -0,0 +1,7 @@
+// This is an _overview_
+//
+package model
+
+// Model is an interface that defines the functions for Model types
+//
+type Model interface{}
---
+++ model/model_test.go
@@ -0,0 +1,8 @@
+package model
+
+import "testing"
+
+// Test if a Model variable can be created
+func TestNewModel(t *testing.T) {
+	var _ Model = nil
+}
