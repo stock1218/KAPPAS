f110d74096710c2cdc2573074e19b9d26e184b4b
John Samuels <johnsamuels1218@gmail.com>
2019-08-06 15:36:43 -0400

    Modify main.go to pass TestStart

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-)
-
 func main() {
-	_, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-	if err != nil {
-		fmt.Println("Error!")
-	} else {
-		fmt.Println("Successfully created a session")
-	}
+
+}
+
+// Create instance of controller for main function
+func start() error {
+	// Create Controller
+	return nil
 }
