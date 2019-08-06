d5887e70ab10793e34c43e028a98779701365445
John Samuels <johnsamuels1218@gmail.com>
2019-08-06 15:40:25 -0400

    Revise documentation for start()

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ func main() {
 
 }
 
-// Create instance of controller for main function
+// Start creates an instance of controller for the main function
 func start() error {
 	// Create Controller
 	return nil
