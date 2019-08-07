7ef8d6d86cd3e131e20e57a6bed409245303d2dc
John Samuels <johnsamuels1218@gmail.com>
2019-08-07 14:05:29 -0400

    Update comment in Model interface

--- model/model.go
+++ model/model.go
@@ -3,7 +3,7 @@ package model
 // Model is an interface that defines the functions for Model types
 //
 type Model interface {
-	PutPAN(string) (string, error) // Put a PAN value into a model
-	GetPAN(string) (string, error) // Get a PAN value in a model
-	encrypt(string) (string, error)
+	PutPAN(string) (string, error)  // Put a PAN value into a model
+	GetPAN(string) (string, error)  // Get a PAN value in a model
+	encrypt(string) (string, error) // Encrypt a string
 }
