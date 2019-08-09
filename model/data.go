ce46827812ced63a3f2604b182afe6a89960f813
John Samuels <johnsamuels1218@gmail.com>
2019-08-09 12:18:32 -0400

    Refactor Data interface to contain required methods

--- model/data.go
+++ model/data.go
@@ -2,4 +2,7 @@ package model
 
 // Data defines an interface describing the methods required for a Data type.
 //
-type Data interface{}
+type Data interface {
+	GetPayload() string // Get the payload contained in Data
+	SetPayload(string)  // Set the payload contained in Data
+}
