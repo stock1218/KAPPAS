4307731f279b10a2f11526adc0f651c9ba7bab9b
John Samuels <johnsamuels1218@gmail.com>
2019-08-09 11:50:54 -0400

    Add godoc to data interface and pan

--- model/data.go
+++ model/data.go
@@ -1,3 +1,5 @@
 package model
 
+// Data defines an interface describing the methods required for a Data type.
+//
 type Data interface{}
--- model/pan.go
+++ model/pan.go
@@ -1,7 +1,11 @@
 package model
 
+// PAN is a Key type that encapsulates PAN data.
+//
 type PAN struct{}
 
+// NewPAN returns a pointer to a new PAN struct.
+//
 func NewPAN() *PAN {
 	return new(PAN)
 }
