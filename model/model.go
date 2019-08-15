b9b4a282fe1c2e77cdc9a3b77f30f1a23e64cbfa
John Samuels <johnsamuels1218@gmail.com>
2019-08-15 17:10:52 -0400

    Fix equal comparitor in TestPutAndGet to correctly compare the put and get value

--- model/model.go
+++ model/model.go
@@ -1,8 +1,10 @@
 package model
 
+import "github.com/stock1218/KAPPAS/model/data"
+
 // Model is an interface that defines the functions for Model types
 //
 type Model interface {
-	PutPAN(string) (string, error) // Put a PAN value into a model
-	GetPAN(string) (string, error) // Get a PAN value in a model
+	PutPAN(*data.PAN) error           // Put a Data value into the AWS model, and update the Data ID
+	GetPAN(string) (data.Data, error) // Get a Data value form the AWS model
 }
