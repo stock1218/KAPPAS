cdaec75106572f58a936ac5659479f8797d9164c
John Samuels <johnsamuels1218@gmail.com>
2019-08-15 10:55:34 -0400

    Add TestNewAmazonRDS to database tests

--- model/database/database_test.go
+++ model/database/database_test.go
@@ -2,8 +2,10 @@ package database
 
 import "testing"
 
-// TestingNewDatabase will test if a Database struct can be created.
+// TestNewDatabase will test if a Database struct can be created.
 //
 func TestNewDatabase(t *testing.T) {
 	var _ Database = nil
 }
+
+// Test
