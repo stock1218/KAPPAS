c0eb568340809cc89b48485c0da9415949e81b71
John Samuels <johnsamuels1218@gmail.com>
2019-08-23 11:44:36 -0400

    Add viewSetup and TestGetObservers, and modify rpc and view to pass

--- view/rpc.go
+++ view/rpc.go
@@ -1,9 +1,18 @@
 package view
 
 // RPC defines a struct for interacting with an RPC interface.
-type RPC struct{}
+type RPC struct {
+	observers []func(string)
+}
 
 // NewRPC creates a new RPC struct.
+//
 func NewRPC() *RPC {
 	return new(RPC)
+}
+
+// GetObservers will return the array of observer functions registered to the RPC.
+//
+func (rpc RPC) GetObservers() []func(string) {
+	return rpc.observers
 }
--- view/view_test.go
+++ view/view_test.go
@@ -3,11 +3,11 @@ package view
 import "testing"
 
 // Test is a View type can be created.
-func TestNewView(t *testing.T) {
+func TestNewview(t *testing.T) {
 	var _ View = nil
 }
 
-// Test is an RPC type View can be created.
+// Test if an RPC type View can be created.
 func TestNewRPC(t *testing.T) {
 	var rpc View = NewRPC()
 
@@ -15,4 +15,16 @@ func TestNewRPC(t *testing.T) {
 		t.Log("View: Failed to create RPC view")
 		t.Fail()
 	}
+}
+
+// A setup function for the following tests to create an RPC struct.
+func viewSetup() *RPC {
+	return NewRPC()
+}
+
+// Test if GetObservers will register an observer to a view type.
+func TestGetObservers(t *testing.T) {
+	testRPC := viewSetup()
+
+	_ = testRPC.GetObservers()
 }
